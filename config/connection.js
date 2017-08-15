f9829d11cb5c650e67ff1a87dbdbd105e5258977
Matt Jiles <mjiles1@gmail.com>
2017-08-14 23:55:54 -0400

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,24 @@
 const mysql = require('mysql2');
+let connection;
 
 if (process.env.CLEARDB_DATABASE_URL) {
-  let connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
-  connection.connect(function (err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-  });
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
 } else {
-  let connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'burgers_db'
   });
-    connection.connect(function (err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-  });
 }
 
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
 
+  console.log('connected as id ' + connection.threadId);
+});
 
 module.exports = connection;
